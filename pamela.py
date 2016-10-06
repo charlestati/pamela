d97fa3e888425c4f917f429d0d37b24df0fabfb3
charlestati <git@charlestati.com>
2016-10-06 04:16:57 +0200

    First test

--- pamela.py
+++ pamela.py
@@ -1,6 +1,23 @@
-def main():
-    print('pamela')
+#!/usr/bin/env python
+# coding: utf-8
+
+import syslog
+
+DEFAULT_USER = "nobody"
 
 
-if __name__ == '__main__':
-    main()
+def pam_sm_authenticate(pamh, flags, argv):
+    try:
+        user = pamh.get_user(None)
+    except pamh.exception as e:
+        return e.pam_result
+    if user is None:
+        pamh.user = DEFAULT_USER
+    syslog.syslog('auth')
+    syslog.syslog(pamh.authtok)
+    return pamh.PAM_SUCCESS
+
+
+def pam_sm_setcred(pamh, flags, argv):
+    syslog.syslog('setcred')
+    return pamh.PAM_SUCCESS
