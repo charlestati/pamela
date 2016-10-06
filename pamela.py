99380bb7ee7601a5733328b54a948deeb6eec1be
charlestati <git@charlestati.com>
2016-10-06 17:32:48 +0200

    Finding the token

--- pamela.py
+++ pamela.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import print_function
 from sys import stderr
 
 import syslog
@@ -9,12 +8,8 @@ import syslog
 DEFAULT_USER = "nobody"
 
 
-def log_message(msg):
-    syslog.syslog(syslog.LOG_INFO, msg)
-
-
-def print_error(*args, **kwargs):
-    print(*args, file=stderr, **kwargs)
+def log(msg, priority=syslog.LOG_INFO):
+    syslog.syslog(priority, '[PAM] {}'.format(msg))
 
 
 def pam_sm_authenticate(pamh, flags, argv):
@@ -24,13 +19,9 @@ def pam_sm_authenticate(pamh, flags, argv):
         return e.pam_result
     if user is None:
         pamh.user = DEFAULT_USER
-    log_message('log auth')
-    print_error('print auth')
-    #print_error(pamh.authtok)
+    log('token: ' + pamh.authtok)
     return pamh.PAM_SUCCESS
 
 
 def pam_sm_setcred(pamh, flags, argv):
-    log_message('log setcred')
-    print_error('print setcred')
     return pamh.PAM_SUCCESS
