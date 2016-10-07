29314bc91b9e0475ede05fac654b08b242de359f
charlestati <git@charlestati.com>
2016-10-07 02:24:09 +0200

    Testing expanduser

--- pamela.py
+++ pamela.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import ConfigParser
+import os
 import syslog
 
 
@@ -8,16 +10,24 @@ def log(msg, priority=syslog.LOG_INFO):
     syslog.syslog(priority, '[PAM] {}'.format(msg))
 
 
-def lock_container(user, token):
+def lock_container():
     log('locking')
     if False:
         raise ValueError('Bad token in lock_container')
 
 
-def unlock_container(user, token):
+def unlock_container(container, mount_point, token):
     log('unlocking')
     if False:
         raise ValueError('Bad token in unlock_container')
+
+
+def get_config(user):
+    config = ConfigParser.ConfigParser()
+    home_dir = os.path.expanduser('~{}'.format(user))
+    config_file = os.path.join(home_dir, '.pamela')
+    config.read(config_file)
+    return config
 
 
 def pam_sm_authenticate(pamh, flags, argv):
@@ -29,9 +39,17 @@ def pam_sm_authenticate(pamh, flags, argv):
     if user is None:
         log('Failed authenticating in pam_sm_authenticate')
         return pamh.PAM_AUTH_ERR
+
+
+    return pamh.PAM_SUCCESS
+
+
     try:
-        unlock_container(user, pamh.authtok)
-    except ValueError:
+        config = get_config(user)
+        for section in config.sections():
+            options = config.options(section)
+            unlock_container(options['container'], options['mountPoint'], pamh.authtok)
+    except ValueError as e:
         log(e)
         return pamh.PAM_AUTHTOK_ERR
     return pamh.PAM_SUCCESS
@@ -45,7 +63,7 @@ def pam_sm_end(pamh):
         return
     if user is not None:
         try:
-            lock_container(user, pamh.authtok)
+            lock_container(user)
         except ValueError as e:
             log(e)
     else:
@@ -53,4 +71,26 @@ def pam_sm_end(pamh):
 
 
 def pam_sm_setcred(pamh, flags, argv):
+
+    log(os.path.expanduser('~'))
+
     return pamh.PAM_SUCCESS
+
+
+def get_abs_path(directory, path):
+    if os.path.isabs(path):
+        return os.path.expanduser(path)
+
+
+# todo Debug
+def main():
+    config = ConfigParser.ConfigParser()
+    config.read('pamela.ini')
+    for section in config.sections():
+        options = config.options(section)
+        container = get_abs_path(config_file, options['container'])
+        #unlock_container(options['container'], options['mountPoint'], 'azerty')
+
+
+if __name__ == '__main__':
+    main()
