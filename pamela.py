7fb07b0a5b3c7444f0f060f1cd7523dd4e895487
charlestati <git@charlestati.com>
2016-10-07 06:02:18 +0200

    Testing paths

--- pamela.py
+++ pamela.py
@@ -1,66 +1,70 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import print_function
+import syslog
 import os
 import ConfigParser
-import syslog
 
 
 def log(msg):
     syslog.syslog('[PAM] {}'.format(msg))
-    #print('[PAM] {}'.format(msg), file=sys.stderr)
 
 
 def lock_container(user):
-    log('locking')
+    log('locking {}'.format(user))
     if False:
         raise ValueError('Bad token in lock_container')
 
 
 def unlock_container(container, mount_point, token):
-    log('unlocking')
+    log('unlocking {} to {}'.format(container, mount_point))
     if False:
         raise ValueError('Bad token in unlock_container')
 
 
-def get_abs_path(directory, path):
-    if os.path.isabs(path):
-        return os.path.expanduser(path)
+def custom_expanduser(path, user):
+    path = os.path.normpath(path)
+    path_parts = path.split(os.sep)
+    for i, part in enumerate(path_parts):
+        if part == '~':
+            path_parts[i] = '~{}'.format(user)
+    expanded_path = os.path.expanduser(os.path.join(*path_parts))
+    return os.path.abspath(expanded_path)
+
+
+def get_section(section, config):
+    containers = {}
+    options = config.options(section)
+    for option in options:
+        containers[option] = config.get(section, option)
+    return containers
 
 
 def get_config(user):
     config = ConfigParser.ConfigParser()
     home_dir = os.path.expanduser('~{}'.format(user))
-    config_file = os.path.join(home_dir, '.pamela')
+    config_file = os.path.join(home_dir, '.pamela.d', 'config.ini')
     config.read(config_file)
     return config
+
+
+def unlock_user(user, token):
+    config = get_config(user)
+    for section in config.sections():
+        options = get_section(section, config)
+        container = custom_expanduser(options['container'], user)
+        mount_point = custom_expanduser(options['mountpoint'], user)
+        unlock_container(container, mount_point, token)
 
 
 def pam_sm_authenticate(pamh, flags, argv):
     try:
         user = pamh.get_user(None)
     except pamh.exception as e:
-        log(e)
         return e.pam_result
     if user is None:
-        log('Failed authenticating in pam_sm_authenticate')
         return pamh.PAM_AUTH_ERR
-
-    log(user)
-    log(os.path.expanduser('~'))
-    log(os.path.expanduser('~{}'.format(user)))
-
-    return pamh.PAM_SUCCESS
-
-    try:
-        config = get_config(user)
-        for section in config.sections():
-            options = config.options(section)
-            unlock_container(options['container'], options['mountPoint'], pamh.authtok)
-    except ValueError as e:
-        log(e)
-        return pamh.PAM_AUTHTOK_ERR
+    unlock_user(user, pamh.authtok)
     return pamh.PAM_SUCCESS
 
 
@@ -68,15 +72,12 @@ def pam_sm_end(pamh):
     try:
         user = pamh.get_user(None)
     except pamh.exception as e:
-        log(e)
         return
     if user is not None:
         try:
             lock_container(user)
         except ValueError as e:
             log(e)
-    else:
-        log('Failed authenticating in pam_sm_end')
 
 
 def pam_sm_setcred(pamh, flags, argv):
